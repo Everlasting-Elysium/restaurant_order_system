7753f62d92e48d35f6d1d6f9e4d6bcb11cf52b12
wuyun <ai_robot@outlook.com>
2024-10-25 12:46:17 +0800

    project init

---
+++ src/main/java/com/elysiam/restaurant_order_system/RestaurantOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.elysiam.restaurant_order_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantOrderSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elysiam/restaurant_order_system/RestaurantOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elysiam.restaurant_order_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantOrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
